719986cd0591c03bf68c9dfbf9e539b767edee53
Audrius Vegys <ave@infare.com>
2023-03-26 22:38:14 +0300

    Added parallel threads

--- Vinted_data_engineering/MapReduce.cs
+++ Vinted_data_engineering/MapReduce.cs
@@ -1,87 +1,78 @@
-﻿using CsvHelper;
-using Microsoft.Office.Interop.Access;
-using System.Data;
-using System.Formats.Asn1;
-using System.Globalization;
-using System.IO;
-using System.Reflection.PortableExecutable;
+﻿using System.Data;
 using System.Text;
 
-string dataFolder = @"C:\Users\ave\Desktop\data";
+string dataFolder = @"C:\Users\ave\Desktop\data";  //Location of data folder
 var clicksFolder = new DirectoryInfo($"{dataFolder}\\clicks").GetFiles();
 var usersFolder = new DirectoryInfo($"{dataFolder}\\users").GetFiles();
-string country = "LT"; // Country filter
-//string country = null;
+string country = null; // Country filter
 
 Dictionary<string, int> dailyClicks = new();
-Dictionary<string, int> dataCollumn = new();
 Dictionary<string, string> idToCountry = new();
 
 if (country != null)
 {
-    foreach (var user in usersFolder)
+    Parallel.ForEach(usersFolder, user =>
     {
-        using (var usersReader = new StreamReader(user.FullName))
-        {
-            int i = 0;
-            var collumn = usersReader.ReadLine().Split(',');
-            foreach (var item in collumn)
-            {
-                dataCollumn[item] = i;
-                i++;
-            }
+        using var usersReader = new StreamReader(user.FullName);
+        int i = 0;
+        AssignCollumnNumber(usersReader.ReadLine().Split(','), i, out Dictionary<string, int> dataCollumn);
 
-            while (usersReader.EndOfStream == false)
+        while (usersReader.EndOfStream == false)
+        {
+            string[] parts = usersReader.ReadLine().Split(',');
+            var requiredPart = parts[dataCollumn["id"]];
+            lock (idToCountry) //Locking dictionary so that parallel tasks wouldn't check/append dictionary at he same time
             {
-                string[] parts = usersReader.ReadLine().Split(',');
-                var requeredPart = parts[dataCollumn["id"]];
-                if (idToCountry.ContainsKey(requeredPart))
+                if (idToCountry.ContainsKey(requiredPart))
                 {
                     continue;
                 }
                 else
                 {
-                    idToCountry[requeredPart] = parts[dataCollumn["country"]];
+                    idToCountry[requiredPart] = parts[dataCollumn["country"]];
                 }
-
             }
-        }
-    }
 
+        }
+    });
 }
 
-foreach (var click in clicksFolder)
+Parallel.ForEach(clicksFolder, clickInfoFile =>
 {
-    using (var reader = new StreamReader(click.FullName))
+    UpdateClickCount(clickInfoFile);
+});
+
+void UpdateClickCount(FileInfo clickInfoFile)
+{
+    using (var reader = new StreamReader(clickInfoFile.FullName))
     {
         int i = 0;
-        foreach (var item in reader.ReadLine().Split(','))
-        {
-            dataCollumn[item] = i;
-            i++;
-        }
+        AssignCollumnNumber(reader.ReadLine().Split(','), i, out Dictionary<string, int> dataCollumn);
 
         while (reader.EndOfStream == false)
         {
             string[] parts = reader.ReadLine().Split(',');
             var requiredPart = parts[dataCollumn["date"]];
             var userId = parts[dataCollumn["user_id"]];
-            if (dailyClicks.ContainsKey(requiredPart) && (string.IsNullOrEmpty(country) || idToCountry.ContainsKey(userId) && idToCountry[userId] == country))
+            lock (dailyClicks)
             {
-                dailyClicks[requiredPart]++;
-            }
-            else
-            {
-                if (string.IsNullOrEmpty(country) || idToCountry.ContainsKey(userId) && idToCountry[userId] == country)
+                if (dailyClicks.ContainsKey(requiredPart) && (string.IsNullOrEmpty(country) || idToCountry.ContainsKey(userId) && idToCountry[userId] == country))
                 {
-                    dailyClicks[requiredPart] = 1;
+                    dailyClicks[requiredPart]++;
                 }
                 else
                 {
-                    if (!dailyClicks.ContainsKey(requiredPart))
+                    if (string.IsNullOrEmpty(country) || idToCountry.ContainsKey(userId) && idToCountry[userId] == country)
                     {
-                        // If there are no clicks from the specified country that day it should still show up in the list with count 0
-                        dailyClicks[requiredPart] = 0;
+                        dailyClicks[requiredPart] = 1;
+                    }
+                    else
+                    {
+                        if (!dailyClicks.ContainsKey(requiredPart))
+                        {
+                            // If there are no clicks from the specified country that day it should still show up in the list with the count 0
+                            dailyClicks[requiredPart] = 0;
+                        }
                     }
                 }
             }
@@ -90,28 +81,39 @@ foreach (var click in clicksFolder)
     }
 }
 
-var path = string.IsNullOrEmpty(country) ? "total_clicks" : "filtered_clicks";
+WriteToCsv(string.IsNullOrEmpty(country), dailyClicks, dataFolder);
 
-DataTable dt = new DataTable { TableName = path };
-dt.Columns.Add("date");
-dt.Columns.Add("count");
-
-foreach (var pair in dailyClicks)
+//Assigning correct collumn number as received data is not always in the same format
+void AssignCollumnNumber(string[] collumn, int i, out Dictionary<string, int> dataCollumn)
 {
-    dt.Rows.Add(pair.Key, pair.Value);
+    dataCollumn = new Dictionary<string, int>();
+    foreach (var item in collumn)
+    {
+        dataCollumn[item] = i;
+        i++;
+    }
 }
 
-StringBuilder sb = new StringBuilder();
-
-string[] columnNames = dt.Columns.Cast<DataColumn>().
-                                  Select(column => column.ColumnName).
-                                  ToArray();
-sb.AppendLine(string.Join(",", columnNames));
-
-foreach (DataRow row in dt.Rows)
+void WriteToCsv(bool hasFilter, Dictionary<string, int> dailyClicks, string dataFolder)
 {
-    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
-    sb.AppendLine(string.Join(",", fields));
-}
+    var path = hasFilter ? "total_clicks" : "filtered_clicks";
 
-File.WriteAllText($"{dataFolder}\\{path}.csv", sb.ToString());
+    DataTable dt = new DataTable { TableName = path };
+    dt.Columns.Add("date");
+    dt.Columns.Add("count");
+    StringBuilder sb = new StringBuilder();
+
+    string[] columnNames = dt.Columns.Cast<DataColumn>().Select(column => column.ColumnName).ToArray();
+    sb.AppendLine(string.Join(",", columnNames));
+
+    foreach (var pair in dailyClicks)
+    {
+        var row = dt.NewRow();
+        row["date"] = pair.Key;
+        row["count"] = pair.Value;
+        string?[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+        sb.AppendLine(string.Join(",", fields));
+    }
+
+    File.WriteAllText($"{dataFolder}\\{path}.csv", sb.ToString());
+}
